02d0b9a0be0796aae6b476b6d1241054e2ecbe1f
TerminalHermit <commit@terminalhermit.dev>
2024-08-21 11:00:30 +0200

    more config
    nvim dep update, ignores

--- bundles/neovim/dot-config/nvim/lua/plugins/init.lua
+++ bundles/neovim/dot-config/nvim/lua/plugins/init.lua
@@ -6,28 +6,16 @@ return {
       require "configs.conform"
     end,
   },
-{
-  "epwalsh/pomo.nvim",
-  version = "*",  -- Recommended, use latest release instead of latest commit
-  lazy = true,
-  cmd = { "TimerStart", "TimerRepeat" },
-  dependencies = {
-    -- Optional, but highly recommended if you want to use the "Default" timer
-    "rcarriga/nvim-notify",
-  },
-  opts = {
-    -- See below for full list of options 👇
-  },
-}
+
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
   -- {
   -- 	"williamboman/mason.nvim",
   -- 	opts = {
